e110135a457095b5cda401231fcff7cf3d7995bc
Доронин Алексей <lyosha.doronin@gmail.com>
2022-07-07 10:36:37 +0200

    Доп логгинг

--- supervisor.py
+++ supervisor.py
@@ -9,6 +9,19 @@ import traceback
 log = logging.getLogger()
 
 class Supervisor:
+    """
+    Accepts any amount of supervised objects. 
+    
+    Objects should have: 
+    
+        run() method for restart in case of error
+        
+        handleTerm() for shutdown sequences. 
+        
+        Both are optional.
+
+    Needs used 'asyncio.EventLoop' object passed as 'ioloop' argument
+    """
     def __init__(self, *args:object, ioloop: asyncio.AbstractEventLoop) -> None:
         self.ioloop = ioloop
         self.obj_list = args
@@ -51,15 +64,26 @@ class Supervisor:
         for task in asyncio.all_tasks():
             task.cancel()
         time.sleep(5)
-        self._runAll()
-    def _runAll(self):
+        self._rerunAll()
+    def runAll(self):
+        for obj in self.obj_list:
+            try:
+                if hasattr(obj,"run") and callable(getattr(obj, "run")):
+                    log.info(f"Starting {obj}...")
+                    obj.run()
+                else:
+                    log.warn(f"Object {obj} does not have a run() method to be initialised!")
+            except:
+                log.error(f"Could not start supervisor client {obj}, full reason:")
+                log.error(traceback.format_exc())
+    def _rerunAll(self):
         for obj in self.obj_list:
             try:
                 if hasattr(obj,"run") and callable(getattr(obj, "run")):
                     log.info(f"Restarting {obj}...")
                     obj.run()
                 else:
-                    log.warn(f"Object {obj} doesnt have a run() method to be restarted on error!")
+                    log.warn(f"Object {obj} does not have a run() method to be restarted on error!")
             except:
-                log.error(f"Could not restart supervisor client {obj} (does it have run() method?), full reason:")
+                log.error(f"Could not restart supervisor client {obj}, full reason:")
                 log.error(traceback.format_exc())
