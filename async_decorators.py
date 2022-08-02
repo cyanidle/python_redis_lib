5601462eab99817800c1045683b39906f9266dfa
Доронин Алексей <lyosha.doronin@gmail.com>
2022-08-02 14:22:47 +0200

    Обновление либы

--- async_decorators.py
+++ async_decorators.py
@@ -6,15 +6,27 @@ import traceback
 import asyncio
 
 class ContinueLoop(Exception):
-    pass
+    """(async_repeating_task) Raise to restart task soon"""
+    def __init__(self, *args: object) -> None:
+        """(async_repeating_task) Raise to restart task soon"""
+        super().__init__(*args)
+    
 class ReturnFromLoop(Exception):
-    pass
+    """(async_repeating_task) Raise to finish task"""
+    def __init__(self, *args: object) -> None:
+        """(async_repeating_task) Raise to finish task"""
+        super().__init__(*args)
+
 class LoopSleep(Exception):
+    """(async_repeating_task) Raise to restart task after delay"""
     def __init__(self, seconds:float, *args: object) -> None:
+        """(async_repeating_task) Raise to restart task after delay"""
         self.seconds = seconds
         super().__init__(*args)
 
-def async_oneshot(* , logger:logging.Logger, on_shutdown = None):
+def async_oneshot(func = None, **kwargs):
+    logger:logging.Logger = kwargs.get("logger") or logging.getLogger() 
+    on_shutdown = kwargs.get("on_shutdown")
     def _async_oneshot(func):
         async def shutdownHook():
             if on_shutdown is None:
@@ -37,7 +49,9 @@ def async_oneshot(* , logger:logging.Logger, on_shutdown = None):
                 logger.error(traceback.format_exc())
                 raise
         return oneshot_wrapper
-    return _async_oneshot
+    if kwargs:
+        return _async_oneshot
+    return _async_oneshot(func)
 
 def async_repeating_task(*, delay:float, on_shutdown = None, logger = logging.getLogger()):
     def _async_repeating_task(func):
@@ -49,13 +63,14 @@ def async_repeating_task(*, delay:float, on_shutdown = None, logger = logging.ge
                 if coroutines.iscoroutine(coro):
                     await coro
         @functools.wraps(func)
-        async def oneshot_wrapper(*args, **kwargs):
+        async def repeating_wrapper(*args, **kwargs):
             try:
                 while True:
                     try:
                         await func(*args, **kwargs)
                         await asyncio.sleep(delay)
                     except ContinueLoop:
+                        await asyncio.sleep(0.01)
                         continue
                     except ReturnFromLoop:
                         return
@@ -73,5 +88,5 @@ def async_repeating_task(*, delay:float, on_shutdown = None, logger = logging.ge
                 logger.error("Error occured:")
                 logger.error(traceback.format_exc())
                 raise
-        return oneshot_wrapper
+        return repeating_wrapper
     return _async_repeating_task
